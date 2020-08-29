de7966accd8642dd17c2ebf4f076931716ad293d
MADHAVA <dasarimadhava810@gmail.com>
2020-08-29 15:14:16 +0530

    instructor console added

---
+++ pages/instructor.vue
@@ -0,0 +1,156 @@
+<template>
+  <div>
+    <v-toolbar>
+      <v-toolbar-title>
+        <h2>Your courses</h2>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn class="white black--text" v-bind="attrs" v-on="on" to>Create Course</v-btn>
+        </template>
+        <span>Add a new course</span>
+      </v-tooltip>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-layout row wrap>
+      <v-flex
+        xs12
+        sm6
+        md4
+        lg3
+        xl3
+        v-for="course in courses"
+        :key="course.id"
+        justify="space-around"
+        ma-5
+      >
+        <v-card class="mx-auto" max-width="344" elevation="20" ripple rounded>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+               <v-btn class="mx-1" absolute fab mt-3 right small dark color="black" v-bind="attrs" v-on="on">
+                <v-icon dark>mdi-pencil</v-icon>
+               </v-btn>
+              </template>
+              <span>edit course</span>
+            </v-tooltip>
+          <nuxt-link to style="color: inherit;text-decoration: none;">
+            <v-img
+              src="https://d3gthpli891tsj.cloudfront.net/wp-content/uploads/2019/01/22063215/GATE-Crash-Course.jpg"
+              height="200px"
+            >
+              
+            </v-img>
+            <v-card-title>{{ course.title }}</v-card-title>
+
+            <v-card-subtitle>You're the Insructor</v-card-subtitle>
+          </nuxt-link>
+          <v-card-actions>
+            <div>ASSIGNMENTS</div>
+            <v-spacer></v-spacer>
+
+            <v-btn icon @click="view(course)">
+              <v-icon>{{ course.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="course.show">
+              <v-data-table
+                :headers="headers"
+                :items="course.assignments"
+                :items-per-page="3"
+                class="elevation-1"
+              ></v-data-table>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // headeres for  assignments table
+      headers: [
+        {
+          text: "Name",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Last Date", value: "lastDate" },
+        { text: "No of submissions", value: "nfs" },
+      ],
+      // sample data
+      // nfs : no of submissions
+      courses: [
+        {
+          id: 1,
+          title: "course 1 ",
+          // for dropdown of assignments
+          show: false,
+          assignments: [
+            {
+              name: "Assignment1 ",
+              lastDate: "15 August 2020",
+              nfs: "10",
+            },
+            {
+              name: "Assignment2",
+              lastDate: "21 August 2020",
+              nfs: "20",
+            },
+            {
+              name: "Assignment3",
+              lastDate: "30 August 2020",
+              nfs: "3",
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: "course 2 ",
+          show: false,
+          assignments: [
+            {
+              name: "Assignment1 ",
+              lastDate: "15 August 2020",
+              nfs: "10",
+            },
+            {
+              name: "Assignment2",
+              lastDate: "21 August 2020",
+              nfs: "20",
+            },
+          ],
+        },
+        {
+          id: 3,
+          title: "course 3 ",
+          show: false,
+          assignments: [],
+        },
+        {
+          id: 4,
+          title: "course 4 ",
+          show: false,
+          assignments: [],
+        },
+      ],
+    };
+  },
+  methods: {
+    //  switch expandable
+    view(course) {
+      course.show = !course.show;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
