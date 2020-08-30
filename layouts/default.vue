a30a8b5e8508e284daf5908b3838e5f6116d2f4a
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-08-30 22:59:26 +0530

    index.vue and default.vue updated
    default.vue removed data that was not needed
    index.vue just a simple hello i.e. cleaned it up

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,8 @@
 <template>
   <v-app dark>
-    <nuxt/>
+    <nuxt />
   </v-app>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
 </script>
