a4a0bf025ff149510cb773e17a8132eaa1763799
dasari810 <dasarimadhava810@gmail.com>
2020-11-15 16:31:37 +0530

    updates create assginment according to updated assignment model

--- pages/createassignment.vue
+++ pages/createassignment.vue
@@ -9,16 +9,30 @@
           <v-divider />
           <v-card-text justify="center">
             <v-form ref="form" v-model="valid" @submit="saveAssignment">
-              <v-text-field
-                v-model="title"
-                label="Title"
-                counter
-                maxlength="100"
-                :rules="[rules.minLength, rules.titleLength]"
-                :outlined="breakpoint"
-                placeholder="Name of the assignment"
-              />
-              <v-text-field
+              <v-row>
+                <v-col sm="6">
+                  <v-text-field
+                    v-model="title"
+                    label="Title"
+                    counter
+                    maxlength="100"
+                    :rules="[rules.minLength, rules.titleLength]"
+                    :outlined="breakpoint"
+                    placeholder="Name of the assignment"
+                  />
+                </v-col>
+                <v-col sm="6">
+                  <v-text-field
+                    v-model.number="weightage"
+                    :rules="[rules.weightageRule]"
+                    label="Task weightage"
+                    :outlined="breakpoint"
+                    required
+                  />
+                </v-col>
+              </v-row>
+
+              <v-textarea
                 v-model="description"
                 label="Description"
                 counter
@@ -44,36 +58,8 @@
           </v-card-text>
           <v-divider />
           <v-card-actions>
-            <small class="ml-2">Points: {{ gettotalPoints }}</small>
             <v-spacer />
             <v-row justify="end" class="mr-1">
-              <v-dialog
-                v-model="dialog"
-                transition="dialog-bottom-transition"
-                :fullscreen="breakpoint"
-                :hide-overlay="breakpoint"
-                persistent
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    color="primary"
-                    text
-                    v-bind="attrs"
-                    dense
-                    @click.stop="dialog = true"
-                    v-on="on"
-                  >
-                    <v-icon>mdi-bookmark-plus-outline</v-icon>Add task
-                  </v-btn>
-                </template>
-                <add-task
-                  :task="getTaskbuff"
-                  :ind="index"
-                  @add:task="addTask"
-                  @discard="discardTask"
-                  @edit:task="modify"
-                />
-              </v-dialog>
               <v-btn
                 color="success"
                 text
@@ -89,67 +75,6 @@
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col v-if="tasks.length > 0" cols="12" md="10" xl="8">
-        <v-slide-y-reverse-transition class="py-1" group tag="v-list" rounded>
-          <template v-for="(task, i) in tasks">
-            <v-list-item :key="i">
-              <v-layout child-flex>
-                <v-card outlined class="my-2">
-                  <v-card-title>
-                    {{ `${i + 1}. ${task.title}` }}
-                  </v-card-title>
-                  <v-card-text>
-                    <v-container>
-                      <v-row>
-                        <div>
-                          <div>
-                            <v-btn outlined color="primary">
-                              <v-icon>mdi-cloud-upload-outline</v-icon>Add file
-                            </v-btn>
-                          </div>
-                          <div>
-                            <small>Allowed types are:
-                              {{ task.allowedTypes.toString() }}</small>
-                          </div>
-                          <div>
-                            <small>Weightage: {{ task.weightage }}</small>
-                          </div>
-                        </div>
-                        <v-divider vertical class="mx-4" />
-                        <div>
-                          <div
-                            v-for="(criterion, ind) in task.reviewAreas"
-                            :key="ind"
-                          >
-                            <v-row class="point-section">
-                              <v-col>
-                                {{ criterion.ques }}
-                                <small>
-                                  <em>{{ criterion.points }} points</em>
-                                </small>
-                              </v-col>
-                            </v-row>
-                          </div>
-                        </div>
-                      </v-row>
-                    </v-container>
-                  </v-card-text>
-                  <v-card-actions>
-                    <small>Allowed file size: {{ task.maximumFileSize }}</small>
-                    <v-spacer />
-                    <v-btn text color="primary" dense @click="editTask(i)">
-                      <v-icon>mdi-clipboard-edit-outline</v-icon>Edit
-                    </v-btn>
-                    <v-btn text color="error" dense @click="deleteTask(i)">
-                      <v-icon>mdi-delete-circle-outline</v-icon>Delete
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-layout>
-            </v-list-item>
-          </template>
-        </v-slide-y-reverse-transition>
-      </v-col>
     </v-row>
     <v-row>
       <v-snackbar v-model="snackbar" app bottom :color="snackbarColor" rounded>
@@ -164,16 +89,15 @@
   </v-container>
 </template>
 <script>
-import AddTask from '@/components/AddTask'
 import DateInput from '@/components/DateInput'
 export default {
   middleware: ['auth'],
   components: {
-    AddTask,
     DateInput
   },
   data () {
     return {
+      weightage: 0,
       snackbar: false,
       snackbarColor: 'error',
       snackbarMessage: '',
@@ -181,52 +105,31 @@ export default {
       valid: false,
       index: -1,
       lastDateOfSub: null,
-      taskbuff: {
-        title: '',
-        allowedTypes: [],
-        maximumFileSize: '5MB',
-        reviewAreas: [],
-        weightage: 0
-      },
-      reviewPhase: false,
-      course: 'Boring Course', // autofill
       title: '',
       description: '',
-      instructor: 'Dusht Aatma', // autofill
-      submissions: [],
       // last date of submission(begining of review)
       submissionDeadline: new Date().toISOString().substr(0, 10),
       // last date of review
       reviewDeadline: new Date().toISOString().substr(0, 10),
-      totalPoints: 0,
-      tasks: [],
-      reviewArea: {
-        task: '',
-        points: 0
-      },
       multipleFiles: false,
       rules: {
         titleLength: value => value.length <= 100 || 'Max 50 characters.',
         descriptionLength: value =>
           value.length <= 250 || 'Max 250 characters.',
-        minLength: value => !!value || 'Required!'
+        minLength: value => !!value || 'Required!',
+        weightageRule (value) {
+          if (
+            /^\d*$/.test(value) &&
+            (value === '' || (parseInt(value) <= 100 && parseInt(value) >= 0))
+          ) { return true } else {
+            return 'Invalid Number'
+          }
+        }
       }
     }
   },
   layout: 'default',
   computed: {
-    // to update the total points of the assignment
-    gettotalPoints () {
-      let sum = 0
-      this.tasks.forEach((element) => {
-        sum += parseInt(element.weightage)
-      })
-      this.updateTotalPoints(sum)
-      return this.totalPoints
-    },
-    getTaskbuff () {
-      return this.taskbuff
-    },
     // required for dialog to be full screen when small screen
     breakpoint () {
       switch (this.$vuetify.breakpoint.name) {
@@ -245,9 +148,6 @@ export default {
     }
   },
   methods: {
-    updateTotalPoints (newTotal) {
-      this.totalPoints = newTotal
-    },
     displaySnackbar (message, color) {
       this.snackbarMessage = message
       this.snackbarColor = color || 'error'
@@ -263,40 +163,6 @@ export default {
         return
       }
       this.displaySnackbar('Ready to save!', 'success')
-    },
-    // during edit one changes the buuffer task which needs to be put back to original state if changes are not saved so this method is
-    discardTask () {
-      this.index = -1
-      this.dialog = false
-      this.taskbuff = {
-        title: '',
-        required: false,
-        allowedTypes: [],
-        maximumFileSize: '5MB',
-        reviewAreas: [],
-        wightage: 0
-      }
-    },
-    // for sending which index is to be edited.
-    editTask (ind) {
-      this.index = ind
-      this.taskbuff = { ...this.tasks[ind] }
-      this.dialog = true
-    },
-    addTask ({ task }) {
-      this.tasks.push(task)
-      this.discardTask()
-    },
-    deleteTask (index) {
-      this.tasks = this.tasks.filter((e, i) => {
-        return i !== index
-      })
-    },
-    // for commiting changes to databases to actual database upon actual editing.
-    modify ({ task, ind }) {
-      // actual editing is done from here editTask method just sets the index for edit.
-      this.tasks[ind] = task
-      this.discardTask()
     }
   }
 }
